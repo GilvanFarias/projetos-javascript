b10c35aaf0c0133a91a5df447f5f0459ef90700c
Gilvan Farias <63085957+GilvanFarias@users.noreply.github.com>
2021-02-11 17:20:42 -0300

    atualizando

--- jogo-do-dinoussaro/script.js
+++ jogo-do-dinoussaro/script.js
@@ -1,12 +1,40 @@
-const dino = document.querySelector(".dino");
+const dino = document.querySelector('.dino');
 
-function pulaDino(event){
+function pula(event){
   if (event.keyCode === 32){
-
-    console.log("Pressionou espaço");
-
+    pula();
   }
 }
 
-document.addEventListener("keyup", pulaDino);
+function pula(){
+  let position = 0;
 
+  let upInterval = setInterval(() => {
+
+    if (position >= 150){
+      clearInterval(upInterval);
+      //descendo
+      let dowInterval = setInterval(() => {
+
+      if(position <= 0){
+          clearInterval(dowInterval);
+      }else{
+
+          position -= 20;
+          dino.style.bottom = position + "px";
+
+        } 
+      }, 10);
+
+    }else{
+
+      //subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+      
+    }
+  
+  }, 30);
+}
+
+document.addEventListener('keyup', pula);
